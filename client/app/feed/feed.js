1cedbdeef637a9dcb94fec9f422318aea0b412da
Justin Webb <jw@justinwebb.io>
2015-05-11 16:58:32 -0700

    Update WorkoutFactory variable references

--- client/app/feed/feed.js
+++ client/app/feed/feed.js
@@ -2,7 +2,7 @@
 * @Author: justinwebb
 * @Date:   2015-05-04 15:54:33
 * @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-11 17:49:37
+* @Last Modified time: 2015-05-11 17:51:16
 */
 
 'use strict';

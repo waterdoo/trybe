0314a58ab76ed704874d69f00f9bc17c0248cd28
VINCE <vtam90@gmail.com>
2015-05-11 16:58:11 -0700

    (feat) Feed redirects user to login if not logged in

--- client/app/feed/feed.js
+++ client/app/feed/feed.js
@@ -2,7 +2,7 @@
 * @Author: justinwebb
 * @Date:   2015-05-04 15:54:33
 * @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-11 17:57:53
+* @Last Modified time: 2015-05-11 17:58:25
 */
 
 'use strict';
@@ -28,9 +28,13 @@
     $scope.data = {};
     $scope.username = AuthFactory.getUsername();
     console.log('Feed username:', $scope.username);
+
     $scope.init = function() {
       if(AuthFactory.isAuth()) {
         console.log('auth found user');
+        $scope.data = {};
+        $scope.username = AuthFactory.getUsername();
+        console.log('Feed username:', $scope.username);
         $scope.getAllWorkouts();
       } else {
         $state.go('login');

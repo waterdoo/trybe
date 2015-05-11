44da675c1e91aea7e997b1d67f0f603e7cb79578
Justin Webb <jw@justinwebb.io>
2015-05-11 12:01:14 -0700

    Connect workout view to Auth and Workout factories

--- client/app/feed/feed.js
+++ client/app/feed/feed.js
@@ -2,7 +2,7 @@
 * @Author: justinwebb
 * @Date:   2015-05-04 15:54:33
 * @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-11 16:57:26
+* @Last Modified time: 2015-05-11 17:24:19
 */
 
 'use strict';

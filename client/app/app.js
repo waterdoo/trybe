96e8de5d67261bfc7345e1cfb7e58349672fd30c
VINCE <vtam90@gmail.com>
2015-05-11 16:53:42 -0700

    (feat) Root and default state set to /feed

--- client/app/app.js
+++ client/app/app.js
@@ -2,7 +2,7 @@
 * @Author: vincetam
 * @Date:   2015-05-05 10:16:27
 * @Last Modified by:   vincetam
-* @Last Modified time: 2015-05-11 17:55:32
+* @Last Modified time: 2015-05-11 17:58:09
 */
 
 'use strict';

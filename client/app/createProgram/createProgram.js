350f99d4e838d989a1f03ef266ebeb6660e38c1d
VINCE <vtam90@gmail.com>
2015-07-03 17:45:02 -0700

    (bug) Metcon, Lift, Benchmark buttons not working in createProgram module. Must implement createProgram factory

--- client/app/createProgram/createProgram.js
+++ client/app/createProgram/createProgram.js
@@ -2,7 +2,7 @@
 * @Author: VINCE
 * @Date:   2015-07-03 17:09:46
 * @Last Modified by:   VINCE
-* @Last Modified time: 2015-07-03 17:22:18
+* @Last Modified time: 2015-07-03 17:41:09
 */
 
 'use strict';
@@ -35,24 +35,11 @@
       $state.go('login');
     } else {
       $scope.username = AuthFactory.getUsername();
-      $scope.isCreatingWorkout = WorkoutFactory.isCreatingWorkout() !== false;
-      $scope.isForProgram = WorkoutFactory.isCreatingForProgram() === true;
-      console.log('workout controller - isForProgram:', $scope.isForProgram);
+      $scope.isEdittingWorkout = WorkoutFactory.isCreatingWorkout() !== false;
+      $scope.isForProgram = true;
     }
 
-    $scope.loadNextWorkout = function() {
-      //Load next workout from user's workout program
-      ProgramFactory.getTrybeWorkouts($scope.username)
-        .then(function(workouts) {
-          $scope.workout = workouts[0];
-          //if no workouts in program, default to blank workout
-          if($scope.workout === undefined) {
-            $scope.createWorkout();
-          }
-        })
-    };
-
-    $scope.createWorkout = function(type) {
+    $scope.initializeWorkout = function(type) {
       type = type || 'metcon';
       $scope.exerciseCount = 0;
       $scope.temp = {};
@@ -71,10 +58,7 @@
 
       //Initialize placeholder suggestions
       $scope.placeholders = {};
-      if(type === 'lift') {
-        $scope.placeholders.instructions = 'Build up to 5-rep max of:';
-        $scope.placeholders.exercise = 'Bench';
-      } else if(type !== 'lift') {
+      if(type !== 'lift') {
         $scope.placeholders.instructions = 'Perform 21, 15, 9 reps of:';
         $scope.placeholders.exercise = 'Pull ups';
       }
@@ -83,6 +67,7 @@
       console.log('workout module obj', $scope.workout);
     };
 
+    //Add exercise to lift workout
     $scope.addExercise = function() {
       //Set ex, sets, and reps to workout obj
       $scope.workout.exercises.push({
@@ -90,18 +75,7 @@
         quantity: [],
         result: null
       });
-
       $scope.exerciseCount++;
-      console.log('updated workouts obj', $scope.workout);
-    };
-
-    $scope.printWorkoutQuantity = function (exercise) {
-      var html = '';
-      var quantity = exercise.quantity; //delete if works
-      if (quantity !== null) {
-        html = quantity[0] +' Sets, ' + quantity[1] + ' Reps';
-      }
-      return html;
     };
 
     $scope.setResultType = function(type) {
@@ -119,36 +93,20 @@
         $scope.workout.finalResult.value = $scope.temp.finalResult;
       }
 
-      //Update workout's username entry, then post
+      //Update workout's username and trybe
       $scope.workout.username = AuthFactory.getUsername();
-
-      //If workout is for posting to user's program, update trybe prop
-      if($scope.isForProgram) {
-        $scope.workout.trybe = $scope.workout.username + 'trybe';
-      } else {
-        $scope.workout.trybe = $scope.workout.username + 'log';
-      }
-
-      //If workout was accepted from program, mark completed so
-      //workout is hidden from program
-      if(!$scope.isForProgram) {
-        ProgramFactory.completeWorkout($scope.workout);
-      }
+      $scope.workout.trybe = $scope.workout.username + 'trybe';
 
       WorkoutFactory.postWorkout($scope.workout);
 
-      //Send user back to either program or log
-      if($scope.isForProgram) {
-        $state.go('program')
-      } else {
-        $state.go('feed');
-      }
+      $state.go('program')
     };
 
     //Initialize workout for log or program
-    if($scope.isCreatingWorkout) {
-      $scope.loadNextWorkout();
+    if($scope.isEdittingWorkout) {
+      $scope.initializeWorkout();
     } else {
+      //TO DO: Load user's selected workout to edit
       $scope.workout = WorkoutFactory.getWorkout();
     }
   };
